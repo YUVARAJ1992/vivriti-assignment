3a294a1a4d1f098fa6e534997c48289d1f2810fd
YUVARAJ1992 <yuvarajatravi@gmail.com>
2023-08-17 15:38:21 +0530

    first commit

---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchProduct = () => {
+    return (
+        <div>
+           <h1>Search Page</h1> 
+        </div>
+    );
+};
+
+export default SearchProduct;
